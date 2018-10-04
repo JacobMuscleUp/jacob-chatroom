8f72638116081c2b81f015b23157725f79bdbf1c
JacobTheProgrammer <jacobtheperson123@hotmail.com>
2018-10-04 22:57:49 +0800

    update

--- index.js
+++ index.js
@@ -55,13 +55,15 @@ mongoClient.connect(
         let chat = mongoClient.db('jacob').collection('chatroom');
 
         socketClient.on('connection', function (socket) {
-            chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
-                if (err) 
-                    throw err;
-                socket.emit('output', {
-                    msgArray: res
+            if (!socket.userName) {
+                chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
+                    if (err) 
+                        throw err;
+                    socket.emit('output', {
+                        msgArray: res
+                    });
                 });
-            });
+            }
             
             socket.on('input', function (data) {
                 const { name, message } = data;
