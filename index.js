bdf929e5048d64e57f0e1e1ca2829a1849a21f02
JacobTheProgrammer <jacobtheperson123@hotmail.com>
2018-10-04 19:00:48 +0800

    initial

---
+++ index.js
@@ -0,0 +1,108 @@
+/*
+const mongoClient = require('mongodb').MongoClient;
+const socketClient = require('socket.io').listen(4000).sockets;
+
+require('dotenv').config();
+
+var numUsers = 0;
+*/
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const path = require('path');
+const mongoClient = require('mongodb').MongoClient;
+const socketClient = require('socket.io')(server);
+
+require('dotenv').config();
+
+var numUsers = 0;
+
+server.listen(3000, () => {
+    console.log('Server listening at port %d', 3000);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoClient.connect(
+    `mongodb://${process.env.USER_NAME}:${process.env.USER_PW}@cluster0-shard-00-00-novoa.mongodb.net:27017,cluster0-shard-00-01-novoa.mongodb.net:27017,cluster0-shard-00-02-novoa.mongodb.net:27017/test?ssl=true&replicaSet=cluster0-shard-0&authSource=admin&retryWrites=true`,
+    { useNewUrlParser: true },
+    function (err, mongoClient) {
+        if (err) 
+            throw err;
+        console.log('database connection established.');
+
+        socketClient.on('connection', function (socket) {
+            let chat = mongoClient.db('jacob').collection('chatroom');
+            
+            chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
+                if (err) 
+                    throw err;
+                socket.emit('output', {
+                    msgArray: res
+                });
+            });
+
+            socket.on('input', function (data) {
+                const { name, message } = data;
+
+                if (name == '' || message == '') {
+                    socket.emit('status', 'Please enter a name and message');
+                } 
+                else {
+                    chat.insert({ name: name, message: message }, function () {
+                        socket.emit('output', {
+                            msgArray: [data]
+                        });
+                        socket.broadcast.emit('output', {
+                            msgArray: [data]
+                        });
+
+                        socket.emit('status', {
+                            message: 'Message sent',
+                            clear: true
+                        });
+                    });
+                }
+            });
+
+            socket.on('add user', function(data) {
+                const { userName } = data;
+                socket.userName = userName;
+                ++numUsers;
+
+                socket.emit('user count updated', {
+                    numUsers: numUsers
+                });
+                socket.broadcast.emit('user count updated', {
+                    numUsers: numUsers
+                });
+
+                socket.broadcast.emit('user added', {
+                    userName: userName
+                });
+            });
+
+            socket.on('disconnect', function() {
+                --numUsers;
+
+                socket.emit('user count updated', {
+                    numUsers: numUsers
+                });
+                socket.broadcast.emit('user count updated', {
+                    numUsers: numUsers
+                });
+
+                socket.broadcast.emit('user removed', {
+                    userName: socket.userName
+                });
+            });
+
+            socket.on('clear history', function (data) {
+                chat.remove({}, function () {
+                    socket.emit('history cleared');
+                    socket.broadcast.emit('history cleared');
+                });
+            });
+        });
+    });
---
+++ public/main.js
@@ -0,0 +1,128 @@
+$(document).ready(function () {
+    const socket = io();
+    //const socket = io('http://localhost:3000');
+    //const socket = io('http://127.0.0.1:4000');
+    if (socket === undefined) return;
+
+    const FADE_TIME = 1000;
+
+    const $window = $(window);
+    const $chatPage = $('.chat.page');
+    const $loginPage = $('.login.page');
+    const $messages = $('.messages');
+    const $inputMessage = $('.inputMessage');
+    const $status = $('.status');
+    const $clearHistory = $('#clearHistory');
+    const $greeting = $('.greeting');
+    const $userCount = $('.userCount');
+
+    var userName;
+
+    $chatPage.fadeOut();
+
+    $window.keydown((event) => {
+        var $userNameInput = $('.userNameInput');
+        if (event.which === 13) {
+            if (userName) {
+                socket.emit('input', {
+                    name: userName,
+                    message: $inputMessage.val()
+                });
+            }
+            else {
+                userName = $userNameInput.val();
+                $chatPage.show();
+                $loginPage.fadeOut();
+                $greeting.text(`welcome to ${userName}`);
+                if (userName !== 'admin')
+                    $clearHistory.fadeOut();
+
+                socket.emit('add user', {
+                    userName: userName
+                });
+            }
+        }
+    });
+
+    $clearHistory.on('click', function () {
+        socket.emit('clear history');
+    });
+
+    socket.on('output', function (data) {
+        const { msgArray } = data;
+        if (msgArray.length) {
+            for (var i = 0; i < msgArray.length; ++i) {
+                addChatMessage(msgArray[i].name, msgArray[i].message);
+                //var message = document.createElement('div');
+                //message.setAttribute('class', 'chat-message');
+                //message.textContent = `${msgArray[i].name}: ${msgArray[i].message}`;
+                //$messages.prepend(message);
+            }
+        }
+    });
+
+    socket.on('status', function (data) {
+        setStatus((typeof data === 'object') ? data.message : data);
+        if (data.clear)
+            $inputMessage.val('');
+    });
+
+    socket.on('history cleared', function () {
+        $messages.empty();
+    });
+
+    socket.on('user count updated', (data) => {
+        const { numUsers } = data;
+        $userCount.text(`${numUsers} ${(numUsers < 2) ? 'user' : 'users'}`);
+    });
+
+    socket.on('user added', (data) => {
+        const { userName } = data;
+        log(`${userName} joined`, { prepend: true });
+    });
+
+    socket.on('user removed', (data) => {
+        const { userName } = data;
+        log(`${userName} left`, { prepend: true });
+    });
+
+    const statusDefault = $status.text();
+    const setStatus = function (s) {
+        $status.text(s).addClass('status');
+
+        if (s !== statusDefault) {
+            var delay = setTimeout(function () {
+                setStatus(statusDefault);
+            }, 4000);
+        }
+    }
+
+    function log(msg, options) {
+        var $elem = $('<li>').text(msg).addClass('log');
+        addMessageElement($elem, options);
+    }
+
+    function addChatMessage(name, msg) {
+        var $elem = $('<li>').text(`${name}: ${msg}`);
+        addMessageElement($elem, { prepend: true });
+    }
+
+    const addMessageElement = (el, options) => {
+        var $el = $(el);
+
+        if (!options)
+            options = {};
+        if (typeof options.fade === 'undefined')
+            options.fade = true;
+        if (typeof options.prepend === 'undefined')
+            options.prepend = false;
+
+        if (options.fade)
+            $el.hide().fadeIn(FADE_TIME);
+
+        if (options.prepend)
+            $messages.prepend($el);
+        else
+            $messages.append($el);
+    }
+});
