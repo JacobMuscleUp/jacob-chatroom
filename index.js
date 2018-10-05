f0b77e0f583c3b9640afdde5bb862b69b5941f6e
JacobTheProgrammer <jacobtheperson123@hotmail.com>
2018-10-05 11:42:50 +0800

    update

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ mongoClient.connect(
 
             socket.on('disconnect', function() {
                 //if (!socket.userName) return; 
+                
                 --numUsers;
 
                 socket.emit('user count updated', {
